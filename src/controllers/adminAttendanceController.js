f2adbb5bc680836e5f343c0300c4de9cf594ea97
kesavanTIT <kesavan.tit@gmail.com>
2026-06-02 15:24:02 +0530

    razorpay testing nowwe

--- src/controllers/adminAttendanceController.js
+++ src/controllers/adminAttendanceController.js
@@ -1,6 +1,6 @@
 const prisma = require("../config/db");
 
-// Helper for parsing global attendance filtersSSSSSSS
+// Helper for parsing global attendance filtersSSSSSSSsssssss
 const buildGlobalFilters = (query) => {
   const { trainerId, courseId, studentId, status, startDate, endDate } = query;
   const filter = {};
